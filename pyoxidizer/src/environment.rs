c86e0f28d1c9481411d6c8ad30cc1f833a383d35
Tamas Nepusz <ntamas@gmail.com>
2024-08-17 02:09:52 +0200

    fix: upgrade minimum Rust version to 1.74.0

--- pyoxidizer/src/environment.rs
+++ pyoxidizer/src/environment.rs
@@ -94,10 +94,10 @@ pub static GIT_SOURCE: Lazy<PyOxidizerSource> = Lazy::new(|| {
 // Remember to update the CI configuration in .github/workflows/
 // and the `Installing Rust` documentation when this changes.
 pub static MINIMUM_RUST_VERSION: Lazy<semver::Version> =
-    Lazy::new(|| semver::Version::new(1, 62, 1));
+    Lazy::new(|| semver::Version::new(1, 74, 0));
 
 /// Version of Rust toolchain to use for our managed Rust install.
-pub const RUST_TOOLCHAIN_VERSION: &str = "1.66.0";
+pub const RUST_TOOLCHAIN_VERSION: &str = "1.74.0";
 
 /// Target triples for Linux.
 pub static LINUX_TARGET_TRIPLES: Lazy<Vec<&'static str>> = Lazy::new(|| {
